6b3012e337e17a469f8b99f9e5469c5ba4d88e9d
Asif <TheAsif01>
2020-04-11 07:39:49 -0700

    Implemented randomNumberWithDigit()

--- 1.js
+++ 1.js
@@ -1,3 +1,9 @@
 function randomNumberInRange(x){
 	return Math.random()*x;
 }
+
+function randomNumberWithDigit(n){
+	var x = "";
+	for(var i=0;i<n;i++) x+=String(Math.round(randomNumberInRange(9)));
+	return x;
+}
