1b14a28d01835bdabda4ff05698de526e395ddd4
Asif <TheAsif01>
2020-04-11 22:34:02 -0700

    Branch Formatted

--- 1.js
+++ 1.js
@@ -1,23 +1,23 @@
-function randomNumberInRange(x){
-	return Math.random()*x;
+function randomNumberInRange(x) {
+  return Math.random() * x;
 }
 
-function randomNumberWithDigit(n){
-	var x = "";
-	for(i=0;i<n;i++) x+=String(Math.round(randomNumberInRange(9)));
-	return x;
+function randomNumberWithDigit(n) {
+  var x = "";
+  for (i = 0; i < n; i++) x += String(Math.round(randomNumberInRange(9)));
+  return x;
 }
 
-function isPrime(x){
-	if(x<2) return false;
-	for(i=3;i<x;i++) if(x%i==0) return false;
-	return true;
+function isPrime(x) {
+  if (x < 2) return false;
+  for (i = 3; i < x; i++) if (x % i == 0) return false;
+  return true;
 }
-function randomPrimeGeneratorInRange(x){
-	while(true){
-		var y = Math.round(randomNumberInRange(x));
-		if(isPrime(y)==true){
-			return y;
-		}
-	}
+function randomPrimeGeneratorInRange(x) {
+  while (true) {
+    var y = Math.round(randomNumberInRange(x));
+    if (isPrime(y) == true) {
+      return y;
+    }
+  }
 }
