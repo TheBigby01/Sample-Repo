efdefb75b22a418b1bfe89485f4dd6a723c0e707
Asif <TheAsif01>
2020-04-11 22:02:02 -0700

    Updated randomPrimeGeneratorInRange()

--- 1.js
+++ 1.js
@@ -7,3 +7,15 @@ function randomNumberWithDigit(n){
 	for(var i=0;i<n;i++) x+=String(Math.round(randomNumberInRange(9)));
 	return x;
 }
+
+function randomPrimeGeneratorInRange(x){
+	while(true){
+		var y = Math.random(randomNumberInRange(x));
+		if(y<2) continue;
+		if(y==2) return true;
+		var isPrime = true;
+		for(i=3;i<y;i++) if(y%i==0) {isPrime = false; break;}
+		if(isPrime) return y;
+		else continue;
+	}
+}
